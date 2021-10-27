568850cd61f88a2dd706d0177359b78c979851e3
qbuser <sharonp@qburst.com>
2021-10-27 17:35:33 +0530

    initial commit

---
+++ JenkinsApp/JenkinsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JenkinsAppApp.swift
+//  JenkinsApp
+//
+//  Created by qbuser on 27/10/21.
+//
+
+import SwiftUI
+
+@main
+struct JenkinsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
